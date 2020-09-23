b34fb9f23b7db6591527606c06f5249dea8b993c
fedorkolmykow <fedorkolmykow@gmail.com>
2020-09-23 09:55:33 +0300

    transfer KeyRunes to model

--- avitly/pkg/service/service.go
+++ avitly/pkg/service/service.go
@@ -4,12 +4,7 @@ import (
 	m "github.com/fedorkolmykow/avitoauto/pkg/models"
 )
 
-var runes = [...]rune{'0','1','2','3','4','5','6','7','8','9',
-	'A','B','C','D','E','F','G','H','I','J','K','L','M',
-	'N', 'O','P','Q','R','S','T','U','V','W','X','Y','Z',
-	'a','b','c','d','e','f','g','h','i','j','k','l','m',
-	'n', 'o','p','q','r','s','t','u','v','w','x','y','z',
-	}
+
 
 type Service interface {
 	SaveURL(Req *m.SaveURLReq) (Resp *m.SaveURLResp, err error)
@@ -26,6 +21,7 @@ type service struct{
 }
 
 func (s *service) SaveURL(Req *m.SaveURLReq) (Resp *m.SaveURLResp, err error){
+
 	return
 }
 
@@ -36,9 +32,9 @@ func (s *service) GetURL(Req *m.RedirectReq) (Resp *m.RedirectResp, err error){
 func shorten(id int) (key string, err error){
 	var keysRunes []rune
 	for id > 0{
-		remain := id % len(runes)
-		keysRunes = append(keysRunes, runes[remain])
-		id = id / len(runes)
+		remain := id % len(m.KeyRunes)
+		keysRunes = append(keysRunes, m.KeyRunes[remain])
+		id = id / len(m.KeyRunes)
 	}
 	key = string(keysRunes)
 	return
