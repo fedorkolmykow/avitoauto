b69bbd1dd39c1893105ef8ce5f3fd50f1cba47fd
fedorkolmykow <fedorkolmykow@gmail.com>
2020-09-23 09:53:05 +0300

    add tests on validation

---
+++ avitly/pkg/models/validation_test.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestCheckKeySuccess(t *testing.T){
+	key := "3vT43"
+	err := checkKey(key)
+	if err != nil{
+		t.Errorf("key %s failed check", key)
+	}
+}
+
+func TestCheckKeyRunes(t *testing.T){
+	key := string(KeyRunes)
+	err := checkKey(key)
+	if err != nil{
+		t.Errorf("key %s failed check", key)
+	}
+}
+
+
+func TestCheckKeyFailed(t *testing.T){
+	key := "РусскийКлюч"
+	err := checkKey(key)
+	if err == nil{
+		t.Errorf("key %s did't fail check", key)
+	}
+}
+
+func TestCheckMixedKeyFailed(t *testing.T){
+	key := "RussianКлюч"
+	err := checkKey(key)
+	if err == nil{
+		t.Errorf("key %s did't fail check", key)
+	}
+}
